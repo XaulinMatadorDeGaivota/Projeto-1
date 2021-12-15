d6069233666cdedafdd47c39446a8f96809ea932
Matheus Cabral Calil <93673317+XaulinMatadorDeGaivota@users.noreply.github.com>
2021-12-15 15:17:53 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,29 +10,54 @@ let world;
 var ball;
 var ground;
 
+var leftSide;
+var rightSide;
+
 function preload(){
-	
+
 }
 
 function setup() {
 	createCanvas(800, 700);
+
+	var Edge1_options={
+		isStatic: true
+	  };
+
+	  var Edge2_options={
+		isStatic: true
+	  };
+
+	leftSide = createSprite(500,650,10,150);
+	rightSide = createSprite(750,650,10,150);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-	var ball_options={
+	var ball_options={	
 		isStatic:false,
 		restitution:0.3,
 		friction:0,
 		density:1.2
 	}
 
-	ground = new Ground(400,790,800,80);
+
+
+	ball=Bodies.circle(100,100,20,ball_options);
+ 	World.add(world,ball);
+
+	ground = new Ground(0,700,1600,10);
 
 	Engine.run(engine);
+
+	Edge1=Bodies.rectangle(500,650,10,150,Edge1_options);
+  	World.add(world,Edge1);
 	
+	Edge2=Bodies.rectangle(750,650,10,150,Edge2_options);
+	World.add(world,Edge2);
+
 	rectMode(CENTER);
 	ellipseMode(RADIUS);
 }
@@ -43,8 +68,28 @@ function draw() {
   background(0);
   ground.show();
 
+  keyPressed();
+
   ellipse(ball.position.x,ball.position.y,20);
 
   drawSprites();
- 
 }
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball,{
+			x:1,y:4
+		  },{
+			x:2,y:3
+		  })
+	}
+}
+	if(keyCode === DOWN_ARROW){
+		if(keyCode === UP_ARROW){
+			Matter.Body.applyForce(ball,{
+				x:-1,y:4
+			  },{
+				x:-2,y:3
+			  })
+		}
+	}
